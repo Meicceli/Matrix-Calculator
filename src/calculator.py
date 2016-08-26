074ca22add82caf91b343bd1eda26be80099fcc0
Marcus Leivo <meicceli@lakka.kapsi.fi>
2016-08-26 19:54:12 +0300

    [calculator.py] Sanitize the code and add more comments.

--- src/calculator.py
+++ src/calculator.py
@@ -78,20 +78,44 @@ def matrixMultiplication(A, B):
     return Matrix(C, n, p)
 
 
+def __reduce_fraction(frac):
+    """Reduce the given fraction."""
+    # Calculate the gcd of the numerator and denominator.
+    syt = my_gcd(frac[0], frac[1])
+    # This condition here ensures we don't end up dividing by 0. Namely, in
+    # Python gcd(0, 0) == 0.
+    if syt == 0:
+        syt = 1
+    # Floor divide the numerator and denominator by syt. We can safely floor
+    # divide, because by the definition of gcd, syt divides frac[0] and frac[1].
+    # Floor division then ensures frac[0] // syt is an integer, since in Python
+    # 3, x / y is by default a float.
+    return (frac[0] // syt, frac[1] // syt)
+
+
 def __pivot(A):
+    """Pivot A so that largest element of each column is on the diagonal."""
     n = A.getRowAmount()
+
+    # At first, P is the identity matrix.
     P = [[int(i == j) for i in my_range(n)] for j in my_range(n)]
+
+    # Store the number of times pivoting is needed here.
     totalPivots = 0
+    # Iterate through columns.
     for j in my_range(n):
-        suurin = 0
+        greatest = 0
         swapWith = j
 
-        for row in my_range(j, n):
-            if my_abs(A.getCell(row, j)) > suurin:
-                suurin = my_abs(A.getCell(row, j))
+        # We don't have to care about column values above the diagonal.
+        for row in my_range(j+1, n):
+            if my_abs(A.getCell(row, j)) > greatest:
+                greatest = my_abs(A.getCell(row, j))
                 swapWith = row
 
+        # True if we need to swap rows.
         if swapWith != j:
+            # Swap rows so that the greatest element is now on the diagonal.
             P[j], P[swapWith] = P[swapWith], P[j]
             totalPivots += 1
 
@@ -101,43 +125,59 @@ def __pivot(A):
 def __LUP_decomposition(A):
     """Calculate the LUP decomposition of A."""
     n = A.getRowAmount()
-    L = [[(int(i == j), 1) for i in my_range(n)] for j in my_range(n)]
+
+    # Format L and U as the zero matrix. Note that we are dealing with fractions
+    # here for 100% accuracy.
+    L = [[(0, 1) for i in my_range(n)] for j in my_range(n)]
     U = [[(0, 1) for i in my_range(n)] for j in my_range(n)]
-    lol = __pivot(A)
-    P = lol[0]
-    # mult == determinant of P
-    mult = lol[1]
+
+    # Pivot A.
+    pivot_info = __pivot(A)
+    P = pivot_info[0]
+
+    # Set mult equal to the determinant of P.
+    mult = pivot_info[1]
     if mult % 2 == 0:
         mult = 1
     else:
         mult = -1
-    Prod = matrixMultiplication(P, A)
-    for j in my_range(n):
-        for i in my_range(j+1):
-            value = (0, 1)
-            for k in my_range(i):
-                toAdd = (L[i][k][0] * U[k][j][0],
-                         L[i][k][1] * U[k][j][1])
-                value = (value[0] * toAdd[1] + toAdd[0] * value[1],
-                         value[1] * toAdd[1])
-                syt = my_gcd(value[0], value[1])
-                if syt == 0:
-                    syt = 1
-                value = (value[0] // syt, value[1] // syt)
-            U[i][j] = (Prod.getCell(i, j) * value[1] - value[0], value[1])
 
+    # This is the matrix P*A
+    Prod = matrixMultiplication(P, A)
+
+    # We use the general algorithm described here:
+    # https://rosettacode.org/wiki/LU_decomposition to calculate cell values for
+    # U and L. There is a summation formula given for U_ij and L_ij, which is
+    # implemented here.
+    for j in my_range(n):
+        # Calculate U[i][j].
+        for i in my_range(j+1):
+            the_sum = (0, 1)
+
+            for k in my_range(i):
+                # U_kj + L_ik
+                toAdd = (U[k][j][0] * L[i][k][0],
+                         U[k][j][1] * L[i][k][1])
+                # Add the fraction toAdd into the_sum
+                the_sum = (the_sum[0] * toAdd[1] + toAdd[0] * the_sum[1],
+                           the_sum[1] * toAdd[1])
+
+            the_sum = __reduce_fraction(the_sum)
+            # U_ij == Prod_ij - the_sum
+            U[i][j] = (Prod.getCell(i, j) * the_sum[1] - the_sum[0], the_sum[1])
+
+        # Calculate L[i][j], similarly as U[i][j].
         for i in my_range(j, n):
-            value = (0, 1)
+            the_sum = (0, 1)
+
             for k in my_range(j):
                 toAdd = (L[i][k][0] * U[k][j][0],
                          L[i][k][1] * U[k][j][1])
-                value = (value[0] * toAdd[1] + toAdd[0] * value[1],
-                         value[1] * toAdd[1])
-                syt = my_gcd(value[0], value[1])
-                if syt == 0:
-                    syt = 1
-                value = (value[0] // syt, value[1] // syt)
-            L[i][j] = (Prod.getCell(i, j) * value[1] - value[0], value[1])
+                the_sum = (the_sum[0] * toAdd[1] + toAdd[0] * the_sum[1],
+                           the_sum[1] * toAdd[1])
+
+            the_sum = __reduce_fraction(the_sum)
+            L[i][j] = (Prod.getCell(i, j) * the_sum[1] - the_sum[0], the_sum[1])
             L[i][j] = (L[i][j][0] * U[j][j][1], L[i][j][1] * U[j][j][0])
 
     return (L, U, P, mult)
@@ -177,42 +217,51 @@ def matrixDeterminant(A):
 
 
 def __forward_substitution(L):
-    m = len(L[0])
-    inverse = [[(0, 1) for i in my_range(m)]
-               for j in my_range(m)]
+    """Invert L using forward substitution.
 
+    For more information, see for example
+    en.wikipedia.org/wiki/Triangular_matrix#Forward_and_back_substitution """
+    m = len(L[0])
+    inverse = [[(0, 1) for i in my_range(m)] for j in my_range(m)]
+
+    # bVector is the a:th column of the identity matrix. Solve the a:th column
+    # of the inverse of L.
     for a in my_range(m):
         bVector = [0 for i in my_range(m)]
         bVector[a] = 1
 
+        # This will be the a:th column of the inverse matrix of L.
         xVector = []
         for x in my_range(m):
-            stuff = (0, 1)
+            # Calculate the summation of x_index using the formula given for x_m
+            # in the article above.
+            the_sum = (0, 1)
+
             for i in my_range(x):
                 toAdd = (L[x][i][0] * xVector[i][0],
                          L[x][i][1] * xVector[i][1])
-                stuff = (stuff[0] * toAdd[1] + toAdd[0] * stuff[1],
-                         stuff[1] * toAdd[1])
-                syt = my_gcd(stuff[0], stuff[1])
-                if syt == 0:
-                    syt = 1
-                stuff = (stuff[0] // syt, stuff[1] // syt)
-            value = (bVector[x] * stuff[1] - stuff[0], stuff[1])
+                the_sum = (the_sum[0] * toAdd[1] + toAdd[0] * the_sum[1],
+                           the_sum[1] * toAdd[1])
+
+            the_sum = __reduce_fraction(the_sum)
+            value = (bVector[x] * the_sum[1] - the_sum[0], the_sum[1])
             value = (value[0] * L[x][x][1], value[1] * L[x][x][0])
-            syt = my_gcd(value[0], value[1])
-            if syt == 0:
-                syt = 1
-            value = (value[0] // syt, value[1] // syt)
+            value = __reduce_fraction(value)
             xVector.append(value)
 
+        # xVector is now the a:th column of L's inverse.
         for i in my_range(m):
             inverse[i][a] = xVector[i]
 
     return inverse
 
 
-def __backward_substitution(L):
-    m = len(L[0])
+def __backward_substitution(U):
+    """Invert U using backward substitution.
+
+    This is basically the same as forward substitution, but done working
+    backwards."""
+    m = len(U[0])
     inverse = [[(0, 1) for i in my_range(m)]
                for j in my_range(m)]
 
@@ -223,23 +272,17 @@ def __backward_substitution(L):
         xVector = []
         for x in my_reversed(my_range(m)):
             stuff = (0, 1)
+
             for i in my_reversed(my_range(x+1, m)):
-                toAdd = (L[x][i][0] * xVector[m-1 - i][0],
-                         L[x][i][1] * xVector[m-1 - i][1])
+                toAdd = (U[x][i][0] * xVector[m-1 - i][0],
+                         U[x][i][1] * xVector[m-1 - i][1])
                 stuff = (stuff[0] * toAdd[1] + toAdd[0] * stuff[1],
                          stuff[1] * toAdd[1])
-                syt = my_gcd(stuff[0], stuff[1])
-                if syt == 0:
-                    syt = 1
-                stuff = (stuff[0] // syt, stuff[1] // syt)
+
+            stuff = __reduce_fraction(stuff)
             value = (bVector[x] * stuff[1] - stuff[0], stuff[1])
-            value = (value[0] * L[x][x][1], value[1] * L[x][x][0])
-            if value[1] == 0:
-                value = (0, 1)
-            syt = my_gcd(value[0], value[1])
-            if syt == 0:
-                syt = 1
-            value = (value[0] // syt, value[1] // syt)
+            value = (value[0] * U[x][x][1], value[1] * U[x][x][0])
+            value = __reduce_fraction(value)
             xVector.append(value)
 
         for i in my_range(m):
@@ -250,37 +293,47 @@ def __backward_substitution(L):
 
 def accurateMatrixInverse(A):
     """Inverse A with 100% accuracy"""
+    # Inverse not defined iff the determinant is zero.
     if matrixDeterminant(A) == 0:
         return None
 
+    # Calculate the LUP decomposition of A.
     decomposition = __LUP_decomposition(A)
 
+    # Inverse L using forward substitution.
     L = __forward_substitution(decomposition[0])
+    # Inverse U using forward substitution.
     U = __backward_substitution(decomposition[1])
     P = decomposition[2]
     n = A.getRowAmount()
 
+    # Calculate the product of L^-1 and U^-1 here.
     C = [[0 for i in my_range(n)] for j in my_range(n)]
 
+    # Do the multiplication.
     for i in my_range(n):
         for j in my_range(n):
             cellValue = (0, 1)
+
             for k in my_range(n):
                 toAdd = (U[i][k][0] * L[k][j][0],
                          U[i][k][1] * L[k][j][1])
                 cellValue = (cellValue[0] * toAdd[1] + toAdd[0] * cellValue[1],
                              cellValue[1] * toAdd[1])
-            syt = my_gcd(cellValue[0], cellValue[1])
-            if syt == 0:
-                syt = 1
-            cellValue = (cellValue[0] // syt, cellValue[1] // syt)
+
+            cellValue = __reduce_fraction(cellValue)
             C[i][j] = cellValue
 
+    # Result is L^-1 * U^-1 = C times the inverse of P. We use the fact that for
+    # permutation matrices, P^-1 == P^T i.e. the inverse of P is it's transpose.
+    # So we multiply C by the transpose of P.
     Result = [[0 for i in my_range(n)] for j in my_range(n)]
     for i in my_range(n):
         for j in my_range(n):
             cellValue = 0
             for k in my_range(n):
+                # Note the order of k and j. The order ensures we are
+                # multiplying by the transpose of P, and not with P.
                 if P.getCell(k, j) == 1:
                     cellValue = C[i][k]
                     break
--- src/myAlgorithms.py
+++ src/myAlgorithms.py
@@ -1,42 +1,78 @@
-from fractions import gcd
-
-
 def my_abs(n):
+    """Calculate the absolute value of a given number."""
     if n < 0:
         return -n
     return n
 
 
 def my_gcd(n, m):
-    """Calculate the gcd of two numbers using the Euclidean algorithm."""
-    return gcd(n, m)
-    if n == m == 0:
-        return 0
-    if n == 0:
-        return m
-    if m == 0:
-        return n
+    """Calculate the gcd of n and m using Stein's algorithm.
 
+    For a description of Stein's algorithm, see for example
+    https://en.wikipedia.org/wiki/Binary_GCD_algorithm
+    """
+
+    # Unit is needed to mimic the default Python's implementation of gcd. Python
+    # thinks that if m < 0, then the gcd shall be negative.
     unit = 1
     if m < 0:
         unit = -1
-
     n = my_abs(n)
     m = my_abs(m)
-    while n != m:
-        if n > m:
-            n -= m
-        else:
-            m -= n
 
-    return n * unit
+    # Base cases.
+    if n == m == 0:
+        return 0
+    if n == 0:
+        return unit * m
+    if m == 0:
+        return n
+    if n == m:
+        return unit * n
+
+    # For convenience, make sure n is the bigger one of the two.
+    if n < m:
+        n, m = m, n
+
+    # The power of two that divides both n and m, i.e. if 8==2^3 divides n and m
+    # but 16==2^4 doesn't, then exp will be set to 3.
+    exp = 0
+    # While n and m are both even, divide both by 2, and add one to exp.
+    while (n | m) & 1 == 0:
+        n >>= 1
+        m >>= 1
+        exp += 1
+
+    # Remove leftover factors of 2 from n. This can be done since m is odd and
+    # so has no factors of 2.
+    while n & 1 == 0:
+        n >>= 1
+
+    while n != 0:
+        # Again, remove any leftover factors of 2 from n since m now always
+        # remains odd.
+        while n & 1 == 0:
+            n >>= 1
+        # Here we ensure that n is always the bigger of the two. Thus n will be
+        # positive until n == m.
+        if n < m:
+            n, m = m, n
+        n -= m
+
+    # At this point, n == 0 and hence gcd(n, m) == m. Multiply the resulting m
+    # by 2^exp, which is also a common factor of the original n and m.
+    return unit * (m << exp)
 
 
 def my_max(x, *args):
+    """Return the maximum of the given numbers or a given list."""
+
+    # If x is a non-empty list, find the maximum value in it.
     if isinstance(x, list) and len(x) > 0:
         greatest = x[0]
         i = 0
         while i < len(x):
+            # The list x must contain only numbers (floats and/or integeres.)
             if not (isinstance(x[i], float) or isinstance(x[i], int)):
                 raise TypeError('Expected an integer or a float.')
             if x[i] > x[0]:
@@ -44,13 +80,17 @@ def my_max(x, *args):
             i += 1
         return greatest
 
+    # If x is a number (float/integer), find the maximum of the given
+    # n-arguments.
     if isinstance(x, float) or isinstance(x, int):
         greatest = x
         i = 0
+        # Iterate through the rest of the arguments given by the user.
         while i < len(args):
+            # All arguments must be numbers.
             if not (isinstance(args[i], float) or isinstance(args[i], int)):
                 raise TypeError('Expected an integer or a float.')
-            if x < args[i]:
+            if args[i] > x:
                 greatest = args[i]
             i += 1
         return greatest
@@ -60,12 +100,19 @@ def my_max(x, *args):
 
 
 def my_range(x, *args):
+    """Return a list of integers, mimicing the default range() call."""
+    # By default, the lower bound is zero.
     lower = 0
+    # By default, the upper bound is x.
     upper = x
+
+    # User gave two arguments i.e. user gave also a lower bound. Then x is the
+    # lower-bound and the second argument (args[0]) is the upper bound.
     if len(args) == 1:
         lower = x
         upper = args[0]
 
+    # Here we generate the range.
     ret = []
     i = lower
     while i < upper:
@@ -76,6 +123,9 @@ def my_range(x, *args):
 
 
 def my_reversed(x):
+    """Reverse a list."""
+
+    # Copy the elements of x to ret, starting from the end of x.
     ret = []
     i = len(x) - 1
     while i >= 0:
