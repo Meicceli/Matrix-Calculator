af85f09f1af551c2ef1b59606de09392c4d776cc
Marcus Leivo <meicceli@lakka.kapsi.fi>
2016-08-31 03:28:30 +0300

    [parser.py] Refractoring and better input value support.

--- src/parser.py
+++ src/parser.py
@@ -8,85 +8,142 @@ if sys.version_info[0] == 2:
     input = raw_input
 
 
+def __ask_input(string):
+    """Ask input from user with 'string'."""
+    try:
+        input_data = input(string)
+    except (KeyboardInterrupt, EOFError):
+        print("\nBye!")
+        sys.exit(0)
+    return input_data
+
+
+def __is_number(n):
+    """Find out if a given argument is a number or not."""
+    try:
+        float(n)
+        return True
+    except:
+        return False
+
+
+def __parse_float(string):
+    """Try parsing a float from string."""
+    elem = my_split(string, ".")
+
+    # String not in the form of 4389.109903
+    if len(elem) == 1:
+        elem = my_split(string, ",")
+
+    # String is of the form 1234,5678
+    if len(elem) == 2:
+        # Both arguments are not numbers.
+        if not (__is_number(elem[0]) and __is_number(elem[1])):
+            return None
+
+        numerator = int(elem[0]) * 10 ** len(elem[1])
+        if numerator >= 0:
+            numerator += int(elem[1])
+        else:
+            numerator -= int(elem[1])
+
+        denominator = 10 ** len(elem[1])
+        frac = (numerator, denominator)
+
+        return frac_reduc(frac)
+
+    # Not a fraction nor a float
+    return None
+
+
+def __parse_fraction(string):
+    """Parse a fraction from a string. Return none if input's not a fraction."""
+    elem = my_split(string, "/")
+
+    if len(elem) == 2:
+        # Do things this weirdly just to handle the case where user gives a
+        # value in the form 1.3/3.7.
+        frac_1 = __parse_float(elem[0])
+        frac_2 = __parse_float(elem[1])
+
+        if not frac_1:
+            if not __is_number(elem[0]):
+                return None
+            frac_1 = (int(elem[0]), 1)
+
+        if not frac_2:
+            if not __is_number(elem[1]):
+                return None
+            frac_2 = (int(elem[1]), 1)
+
+        numerator = frac_1[0] * frac_2[1]
+        denominator = frac_1[1] * frac_2[0]
+        frac = (numerator, denominator)
+        return frac_reduc(frac)
+
+    return None
+
+
+def __parse_values(row):
+    """Parse numbers from a string into a list."""
+    values = []
+    args = my_split(row, " ")
+
+    for i in range(len(args)):
+        elem = __parse_fraction(args[i])
+
+        if not elem:  # elem is not a fraction
+            elem = __parse_float(args[i])
+
+        if not elem:  # elem is not a float nor a fraction
+            if not __is_number(args[i]):  # elem is not even a number
+                return None
+            elem = (int(args[i]), 1)
+
+        values.append(elem)
+
+    return values
+
+
 def parseMatrix():
     """Ask user to input a matrix. Then, create a new Matrix object."""
 
     rows = []
-    rowAmount = 0
 
     print("")
     print("Input a matrix row by row. Plain enter stops.")
 
-    # Handle possible Ctrl-c
-    try:
-        row = input("row: ")
-    except KeyboardInterrupt:
-        print("\nBye!")
-        sys.exit(0)
+    row = my_strip(__ask_input("row: "))
 
     # Loop until an empty row is encountered.
     while row:
-        try:
-            newRow = []
-            args = my_split(row, " ")
-            for i in range(len(args)):
-                elem = my_split(args[i], "/")
-                # elem is not a fraction
-                if len(elem) == 1:
-                    elem = my_split(args[i], ".")
-                    # elem is not a float (e.g. it is not 23.0329857)
-                    if len(elem) == 1:
-                        newRow.append((int(elem[0]), 1))
-                        continue
-                    # elem is a float
-                    if len(elem) == 2:
-                        numerator = int(elem[0]+elem[1])
-                        numerator *= 10 ** (len(elem[1]) - 1)
-                        denominator = 10 ** len(elem[1])
-                        frac = frac_reduc((numerator, denominator))
-                        newRow.append(frac)
-                        continue
-                    # I don't know what the heck elem is
-                    if len(elem) > 2:
-                        raise ValueError
-                # elem is a fraction
-                if (len(elem) == 2):
-                    numerator = int(elem[0])
-                    denominator = int(elem[1])
-                    newRow.append(frac_reduc((numerator, denominator)))
-                    continue
-                # I don't know what the heck elem is
-                if len(elem) > 2:
-                    raise ValueError
-            # User gives more values than there is in the first row.
-            if len(rows) > 0 and len(newRow) != len(rows[0]):
-                raise SyntaxError
-        # User inputs a non-number.
-        except ValueError:
+        newRow = __parse_values(row)
+
+        # User inputs an invalid argument e.g. an English word.
+        if not newRow:
             print("")
-            print("You inputted a non-number. Don't do that.")
+            print("You inputted an invalid argument. Don't do that.")
             print("Please, input the row correctly.")
-        except SyntaxError:
+            print("Only integers, fractions and floats (decimals) "
+                  "are accepted. No parenthesis please.")
+
+        # User gives more values than there is in the first row.
+        elif len(rows) > 0 and len(newRow) != len(rows[0]):
             print("")
             print("You inputted an invalid amount of numbers.")
             print("Please, input the row correctly.")
-        # If user inputs a row correctly, add it to the rows array.
+
         else:
             rows.append(newRow)
-            rowAmount += 1
 
-        # Handle possible Ctrl-c
-        try:
-            row = my_strip(input("row: "))
-        except KeyboardInterrupt:
-            print("\nBye!")
-            sys.exit(0)
+        row = my_strip(__ask_input("row: "))
 
     # No rows given.
     if len(rows) == 0:
         return None
 
-    return Matrix(rows, len(rows), len(rows[0]))
+    ret = Matrix(rows, len(rows), len(rows[0]))
+    return ret
 
 
 def parseOperator():
@@ -110,12 +167,7 @@ def parseOperator():
 
     print("")
 
-    # Handle possible Ctrl-c
-    try:
-        operator = my_lower(my_strip(input("Operator: ")))
-    except KeyboardInterrupt:
-        print("\nBye!")
-        sys.exit(0)
+    operator = my_lower(my_strip(__ask_input("Operator: ")))
     while operator not in ["*",
                            "-",
                            "+",
@@ -127,55 +179,38 @@ def parseOperator():
                            "print",
                            "transpose"]:
         print("Please choose a valid operator.")
-        # Handle possible Ctrl-c
-        try:
-            operator = my_lower(my_strip(input("Operator: ")))
-        except KeyboardInterrupt:
-            print("\nBye!")
-            sys.exit(0)
+        operator = my_lower(my_strip(__ask_input("Operator: ")))
 
     return operator
-
-
-def __is_number(n):
-    """Find out if a given argument is a number or not."""
-    return isinstance(n, float) or isinstance(n, int)
 
 
 def parseScalar():
     """Ask a scalar multiplicator from user."""
     print("")
 
-    try:
-        scalar = input("Input an integer scalar value: ")
-    except KeyboardInterrupt:
-        print("\nBye!")
-        sys.exit(0)
+    scalar = __ask_input("Input an integer scalar value: ")
 
     # User must give an integer as a scalar.
-    while not __is_number(scalar) or float(scalar) % 1 != 0:
-        try:
-            scalar = input("Input a proper integer scalar value: ")
-        except KeyboardInterrupt:
-            print("\nBye!")
-            sys.exit(0)
-    return int(scalar)
+    while not __is_number(scalar):
+        scalar = __ask_input("Input an integer scalar value: ")
+
+    if scalar // 1 == scalar:
+        return int(scalar)
+    return scalar
 
 
 def askToContinue():
     """Ask the user to continue performing operations to the current matrix."""
     print("")
     print("Do you want to apply more options to the current matrix?")
-    try:
-        a = input("Y/N: ")
-    except KeyboardInterrupt:
-        print("\nBye!")
-        sys.exit(0)
+    a = my_lower(__ask_input("Y/N: "))
     print("")
 
-    # User wants to continue.
-    if my_lower(a) == "y":
-        return True
+    while(a != "y" and a != "n"):
+        a = my_lower(__ask_input("Y/N: "))
 
+    # User wants to continue.
+    if a == "y":
+        return True
     # User does not want to continue.
     return False
